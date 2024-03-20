eebf5e86a6e45366356bd8dbe297958df74d1dd3
legogoUA <olehmedushivskyi@gmail.com>
2024-03-20 17:58:25 +0200

    add notes about alert, prompt, confirm

--- src/app.js
+++ src/app.js
@@ -1,15 +1,20 @@
-let firstNumber = 10;
-let secondNumber = 15;
-let sumNumber = firstNumber + secondNumber;
+// let firstNumber = 10;
+// let secondNumber = 15;
+// let sumNumber = firstNumber + secondNumber;
 
-console.log(sumNumber);
+// console.log(sumNumber);
 
-let admin;
-let name;
+// let admin;
+// let name1;
 
-name = 'Name';
-admin = name;
+// name = 'Name';
+// admin = name;
 
-alert(admin);
+// alert(admin);
 
-let currentUser = 'Name';
+// let currentUser = 'Name';
+
+
+let name = prompt('Enter your name', '');
+alert(`Hi,${name}! Nice to meet you!`);
+// alert, prompt, confirm
